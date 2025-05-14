a9c3d7f8fcac4a6d69ab8163db3491e21931f69a
spascarella <salvatore.pascarella01@gmail.com>
2025-05-14 23:29:49 +0200

    Removed tanh from NN forward method

--- controller.py
+++ controller.py
@@ -19,7 +19,7 @@ class RobotBrain(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.tanh(self.fc3(x))
+        x = self.fc3(x)
         return x
 
     def predict(self, state):
